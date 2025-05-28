9e7e54da237c75e5419adb6b804e8001ed4eff40
otabekturayev <turayevotabek195@gmail.com>
2025-05-28 22:30:05 +0500

    add: price

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { MdDone } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
 
 const Price = () => {
   return (
-    <section className="bg-[#000000de] pt-[50px] pb-[300px] rounded-t-[50px]">
+    <section className="bg-[#000000de] pt-[50px] h-[550px]  rounded-t-[50px] mb-[400px]">
       <div className="container">
         <div>
           <div className="w-full text-center ">
@@ -13,6 +15,81 @@ const Price = () => {
               Exercitation aliquip cillum velit ut tempor aliqua. Reprehenderit exercitation
             </p>
           </div>
+        </div>
+        <div className="bg-[#fff] rounded-t-[45px] grid grid-cols-3 gap-3 p-[50px] mt-[50px]">
+            <div className="border border-gray-300 rounded-[40px]">
+                <div className="bg-gray-100 rounded-t-[40px] p-[40px]">
+                    <div className="font-[600]">
+                        Starter
+                    </div>
+                    <div className="font-[700] text-[40px] mt-[15px]">
+                        Free
+                    </div>
+                    <div className="mt-[15px] text-gray-500">
+                        Perfect for personal use
+                    </div>
+                </div>
+                <div className="p-[40px] flex flex-col gap-[20px]">
+                    <ul className="flex flex-col gap-[10px]">
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                    </ul>
+                    <button className="bg-[#386d63] cursor-pointer w-full text-[#fff] text-[20px] font-[600] rounded-[30px] py-[13px]"><a href="#">Try For Free</a></button>
+                </div>
+            </div>
+            <div className="border border-gray-300 rounded-[40px]">
+                <div className="bg-[#000] rounded-t-[40px] p-[40px] text-[#fff]">
+                    <div className="font-[600]">
+                        Starter
+                    </div>
+                    <div className="font-[700] text-[40px] mt-[15px]">
+                        Free
+                    </div>
+                    <div className="mt-[15px]">
+                        Perfect for personal use
+                    </div>
+                </div>
+                <div className="p-[40px] flex flex-col gap-[20px]">
+                    <ul className="flex flex-col gap-[10px]">
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                    </ul>
+                    <button className="bg-[#000] cursor-pointer w-full text-[#fff] text-[20px] font-[600] rounded-[30px] py-[13px]"><a href="#">Try For Free</a></button>
+                </div>
+            </div>
+            <div className="border border-gray-300 rounded-[40px]">
+                <div className="bg-gray-100 rounded-t-[40px] p-[40px]">
+                    <div className="font-[600]">
+                        Starter
+                    </div>
+                    <div className="font-[700] text-[40px] mt-[15px]">
+                        Free
+                    </div>
+                    <div className="mt-[15px] text-gray-500">
+                        Perfect for personal use
+                    </div>
+                </div>
+                <div className="p-[40px] flex flex-col gap-[20px]">
+                    <ul className="flex flex-col gap-[10px]">
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><MdDone className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                        <li className="flex items-center gap-[15px] text-[20px] font-[500]"><RxCross2 className="text-[#386d63]"/> <span>1 User Only</span></li>
+                    </ul>
+                    <button className="bg-[#386d63] cursor-pointer w-full text-[#fff] text-[20px] font-[600] rounded-[30px] py-[13px]"><a href="#">Try For Free</a></button>
+                </div>
+            </div>
+            
         </div>
       </div>
     </section>
